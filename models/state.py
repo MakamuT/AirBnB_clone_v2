c7889ae322e892e6f9a24a2af5ebf3a09d046168
MakamuT <makamuthe2@gmail.com>
2024-06-03 14:44:04 +0000

    sqlachemy

--- models/state.py
+++ models/state.py
@@ -14,12 +14,20 @@ if models.storage_type == "db":
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", cascade="all, delete-orphan", backref="state")
     else:
-        @property
-        def cities(self):
-            """getter attribute cities that returns the list of City"""
-            from models import storage
-            cities_list = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
+	name = ""
+
+	def __init__(self, *args, **kwargs):
+            """initialize state"""
+            super().__init__(*args, **kwargs)
+
+        if models.storage_t != "db":
+           @property
+           def cities(self):
+               """getter attribute cities that returns the list of City"""
+               from models import storage
+               cities_list = []
+               all_cities = models.storage.all(City)
+               for city in storage.all(City).values():
+                   if city.state_id == self.id:
+                      cities_list.append(city)
+               return cities_list
