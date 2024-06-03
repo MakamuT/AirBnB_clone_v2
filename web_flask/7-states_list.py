e0a5444e4a4f5e88ec966eec48ca61c87274aa78
MakamuT <makamuthe2@gmail.com>
2024-06-03 15:03:57 +0000

    flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,7 +18,7 @@ def states_list():
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """closes"""
+    """closes the storage on teardown"""
     storage.close()
 
 if __name__ == '__main__':
